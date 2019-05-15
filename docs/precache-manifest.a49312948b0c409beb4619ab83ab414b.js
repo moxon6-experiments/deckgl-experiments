8dd7f5129164f26995c1177b46ea75d147619de7
Martin Moxon <moxon6@googlemail.com>
2019-05-15 17:25:42 +0100

    Add docs

---
+++ docs/precache-manifest.a49312948b0c409beb4619ab83ab414b.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e5ba5b1b66b2f89fea1ac34b1a8f9c2",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ad40a057f9a3c00b8be5.bundle.js"
+  },
+  {
+    "url": "runtime~main.ad40a057f9a3c00b8be5.bundle.js"
+  },
+  {
+    "url": "vendors~main.ad40a057f9a3c00b8be5.bundle.js"
+  }
+]);

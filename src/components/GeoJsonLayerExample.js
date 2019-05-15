15f2488616959b8053dc1939f240dc9feb9b2c6f
Martin Moxon <moxon6@googlemail.com>
2019-05-15 16:03:16 +0100

    Two examples

---
+++ src/components/GeoJsonLayerExample.js
@@ -0,0 +1,89 @@
+import React, {Component} from 'react';
+import {StaticMap} from 'react-map-gl';
+import DeckGL, {GeoJsonLayer} from 'deck.gl';
+import {scaleThreshold} from 'd3-scale';
+
+// Set your mapbox token here
+const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
+
+// Source data GeoJSON
+const DATA_URL =
+  'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/geojson/vancouver-blocks.json'; // eslint-disable-line
+
+export const COLOR_SCALE = scaleThreshold()
+  .domain([-0.6, -0.45, -0.3, -0.15, 0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.05, 1.2])
+  .range([
+    [65, 182, 196],
+    [127, 205, 187],
+    [199, 233, 180],
+    [237, 248, 177],
+    // zero
+    [255, 255, 204],
+    [255, 237, 160],
+    [254, 217, 118],
+    [254, 178, 76],
+    [253, 141, 60],
+    [252, 78, 42],
+    [227, 26, 28],
+    [189, 0, 38],
+    [128, 0, 38]
+  ]);
+
+export const INITIAL_VIEW_STATE = {
+  latitude: 49.254,
+  longitude: -123.13,
+  zoom: 11,
+  maxZoom: 16,
+  pitch: 45,
+  bearing: 0
+};
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hoveredObject: null
+    };
+  }
+
+
+
+  _renderLayers() {
+    const {data = DATA_URL} = this.props;
+
+    return [
+      new GeoJsonLayer({
+        id: 'geojson',
+        data,
+        opacity: 0.8,
+        stroked: false,
+        filled: true,
+        extruded: true,
+        wireframe: true,
+        fp64: true,
+        getElevation: f => Math.sqrt(f.properties.valuePerSqm) * 10,
+        getFillColor: f => COLOR_SCALE(f.properties.growth),
+        getLineColor: [255, 255, 255],
+        pickable: true,
+      })
+    ];
+  }
+
+  render() {
+    const {viewState, controller = true, baseMap = true} = this.props;
+
+    return (
+      <DeckGL
+        layers={this._renderLayers()}
+        initialViewState={INITIAL_VIEW_STATE}
+        viewState={viewState}
+        controller={controller}
+      >
+      </DeckGL>
+    );
+  }
+}
+
+
+export default () => <App />
